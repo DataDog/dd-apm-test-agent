947cf75fd30c112a50fb3788adaf87832fc1d935
kyle-verhoog <kyle@verhoog.ca>
2020-06-08 00:41:45 -0400

    fmt

--- src/dbug_agent/handler.clj
+++ src/dbug_agent/handler.clj
@@ -3,8 +3,7 @@
             [compojure.route :as route]
             [msgpack.core :as msg]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [ring.middleware.reload :refer [wrap-reload]]
-            ))
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn trace-db-validator [db] true)
 
@@ -16,8 +15,7 @@
 (add-watch trace-db
            :logger
            (fn [key watched old-state new-state]
-             (println new-state)
-             ))
+             (println new-state)))
 
 (defn db-add-traces
   [token traces]
@@ -31,93 +29,66 @@
    (if (empty? spans)
      traces
      (let
-       [span (first spans)]
-       (parse-spans (rest spans) (assoc-in traces [(span "trace_id") (span "span_id")] span)))
-     )))
-
+      [span (first spans)]
+       (parse-spans (rest spans) (assoc-in traces [(span "trace_id") (span "span_id")] span))))))
 
 (defn parse-traces
   ([raw-traces]
    (do
      ;; check for empty traces
      (when (not-empty (filter empty? raw-traces))
-       (throw (ex-info (format "Empty traces from client") {}))
-       )
+       (throw (ex-info (format "Empty traces from client") {})))
 
      ;; checks done on each raw trace that was received
      (defn raw-trace-check [raw-trace]
-       (let [
-             trace-ids (set (map #(% "trace_id") raw-trace))
-             span-ids (map #(% "span_id") raw-trace)
-             ]
+       (let [trace-ids (set (map #(% "trace_id") raw-trace))
+             span-ids (map #(% "span_id") raw-trace)]
 
          ;; ensure no collisions in span ids
          (when (not= (count span-ids) (count (set span-ids)))
-            (throw (ex-info (format "Collision in span ids for trace trace") {})))
+           (throw (ex-info (format "Collision in span ids for trace trace") {})))
 
          ;; check for mismatching trace ids in a trace
          (when (> (count trace-ids) 1)
-            (throw (ex-info (format "Multiple trace ids in trace %s" trace-ids) {})))))
+           (throw (ex-info (format "Multiple trace ids in trace %s" trace-ids) {})))))
      (doall (map raw-trace-check raw-traces))
 
      ;; collect traces together since traces can be fragmented
      (def traces (parse-spans (reduce concat raw-traces)))
 
      ;; check that all referenced spans exist (TODO distributed tracing issues?)
-     traces
-     )))
-
+     traces)))
 
 (defn trace-check [token]
-  (let [
-        raw-traces (or (@trace-db token) (throw (ex-info (format "Token '%s' not found" token) {})))
-        parsed-traces (parse-traces raw-traces)
-        ]
-    raw-traces
-    )
-  )
-
+  (let [raw-traces (or (@trace-db token) (throw (ex-info (format "Token '%s' not found" token) {})))
+        parsed-traces (parse-traces raw-traces)]
+    raw-traces))
 
 (defn mw-encoding [handler]
   (fn [req]
-    (let [
-          encoding (get-in req [:headers "content-type"])
+    (let [encoding (get-in req [:headers "content-type"])
           raw-body (get req :body)
           body (cond
-            (= encoding "application/msgpack") (msg/unpack raw-body)
-            (= encoding "application/json") nil  ; TODO support json
-            :else nil
-            )
-          ]
-      (handler (assoc req :body body))
-      )))
+                 (= encoding "application/msgpack") (msg/unpack raw-body)
+                 (= encoding "application/json") nil  ; TODO support json
+                 :else nil)]
 
+      (handler (assoc req :body body)))))
 
 (defn handle-traces [req]
-  (let [
-        token (get-in req [:headers "x-datadog-test-token"] "none")
-        traces (get req :body)
-        ]
+  (let [token (get-in req [:headers "x-datadog-test-token"] "none")
+        traces (get req :body)]
     (db-add-traces token traces)
-    {:status 200 :headers {"Content-Type" "application/json"} :body "\"OK\""}
-    ))
-
+    {:status 200 :headers {"Content-Type" "application/json"} :body "\"OK\""}))
 
 (defn handle-check [req]
-  (let [
-        token (get-in req [:headers "x-datadog-test-token"] "none")
-        ]
+  (let [token (get-in req [:headers "x-datadog-test-token"] "none")]
     (try
       (let
-        [traces (trace-check token)]
-        {:status 200 :headers {"Content-Type" "application/json"} :body traces}
-        )
+       [traces (trace-check token)]
+        {:status 200 :headers {"Content-Type" "application/json"} :body traces})
       (catch clojure.lang.ExceptionInfo e
-          {:status 500 :headers {"Content-Type" "application/json"} :body (.getMessage e)}
-        )
-      )
-    )
-  )
+        {:status 500 :headers {"Content-Type" "application/json"} :body (.getMessage e)}))))
 
 (defroutes app-routes
   (mw-encoding (PUT "/v0.4/traces" [] handle-traces))
