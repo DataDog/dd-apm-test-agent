90221edd199aa942e1ab10f43a0e2a13d2593403
Kyle Verhoog <kyle@verhoog.ca>
2022-05-26 21:25:24 -0400

    Serialize ddsketch in trace stats snapshots
    Serialize the sketches to dictionaries.
    
    Right now sketches are stored in snapshots but are serialized to just
    their length in bytes. This doesn't capture any meaningful information
    about the sketches to ensure that they aren't changing.
    
    The issue is that the buckets of the ddsketch are potentially quite noisy so
    really the only information that can be captured and compared are the
    settings of the ddsketch.

--- ddapm_test_agent/tracestats.py
+++ ddapm_test_agent/tracestats.py
@@ -4,6 +4,7 @@ from typing import TypedDict
 
 from ddsketch import DDSketch
 from ddsketch.pb.ddsketch_pb2 import DDSketch as DDSketchProto
+from google.protobuf.json_format import MessageToDict
 import msgpack
 
 
@@ -43,6 +44,10 @@ def decode_v06(data: bytes) -> v06StatsPayload:
     for raw_bucket in payload["Stats"]:
         stats: List[StatsAggr] = []
         for raw_stats in raw_bucket["Stats"]:
+            ok_summary = DDSketchProto()
+            ok_summary.ParseFromString(raw_stats["OkSummary"])
+            err_summary = DDSketchProto()
+            err_summary.ParseFromString(raw_stats["ErrorSummary"])
             stat = StatsAggr(
                 Name=raw_stats["Name"],
                 Resource=raw_stats["Resource"],
@@ -53,8 +58,8 @@ def decode_v06(data: bytes) -> v06StatsPayload:
                 TopLevelHits=raw_stats["TopLevelHits"],
                 Duration=raw_stats["Duration"],
                 Errors=raw_stats["Errors"],
-                OkSummary=DDSketchProto().ParseFromString(raw_stats["OkSummary"]),
-                ErrorSummary=DDSketchProto().ParseFromString(raw_stats["ErrorSummary"]),
+                OkSummary=MessageToDict(ok_summary),
+                ErrorSummary=MessageToDict(err_summary),
             )
             stats.append(stat)
 
