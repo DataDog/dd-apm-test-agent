8af4518f61c2767bcde401a0b43f586541030bb2
colin-higgins <colin@COLIN-X1.localdomain>
2022-06-01 15:49:20 -0400

    Actual formatting

--- ddapm_test_agent/agent.py
+++ ddapm_test_agent/agent.py
@@ -362,7 +362,8 @@ class Agent:
                 with open(tracestats_snap_file, mode="r") as f:
                     raw_snapshot = json.load(f)
                 tracestats_snapshot.snapshot(
-                    expected_stats=raw_snapshot, received_stats=received_stats,
+                    expected_stats=raw_snapshot,
+                    received_stats=received_stats,
                 )
             elif received_stats:
                 # Create a new snapshot for the data received
@@ -469,7 +470,10 @@ def make_app(
     agent = Agent()
     app = web.Application(
         client_max_size=int(100e6),  # 100MB - arbitrary
-        middlewares=[check_failure_middleware, session_token_middleware,],
+        middlewares=[
+            check_failure_middleware,
+            session_token_middleware,
+        ],
     )
     app.add_routes(
         [
@@ -510,7 +514,8 @@ def main(args: Optional[List[str]] = None) -> None:
     if args is None:
         args = sys.argv[1:]
     parser = argparse.ArgumentParser(
-        description="Datadog APM test agent", prog="ddapm-test-agent",
+        description="Datadog APM test agent",
+        prog="ddapm-test-agent",
     )
     parser.add_argument(
         "-v",
