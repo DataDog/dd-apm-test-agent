368f2f07eff11a6d58c935c2fb0414152257ca51
wconti27 <william.conti@datadoghq.com>
2023-01-20 14:28:58 -0500

    Add to whitelist

--- ddapm_test_agent/span_validation/tag_rules/whitelist.py
+++ ddapm_test_agent/span_validation/tag_rules/whitelist.py
@@ -1,1 +1,14 @@
-span_whitelist = ["TCPConnector.connect", "parent", "redis_get"]
+span_whitelist = [
+    "TCPConnector.connect",
+    "parent",
+    "redis_get",
+    "random_sleep",
+    "child",
+    "web-request",
+    "snowflake_op",
+    "test-telemetry",
+    "parent2",
+    "test_middleware",
+    "test-execute-instrumentation",
+    "operation",
+]
