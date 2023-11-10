65441b7547399591af1decf6a7ec535c6dc85de3
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2023-11-10 10:58:57 +0000

    Add `/test/session/tracerflares` endpoint to return all tracer-flares received by the test agent for a given session token.

---
+++ ddapm_test_agent/tracerflare.py
@@ -0,0 +1,27 @@
+from asyncio import StreamReader
+import base64
+from typing import Dict
+from typing import Mapping
+
+from aiohttp import MultipartReader
+
+
+TracerFlareEvent = Dict[str, str]
+
+
+async def v1_decode(headers: Mapping[str, str], data: bytes) -> TracerFlareEvent:
+    """Decode v1 tracer flare form as a dict"""
+    tracer_flare: TracerFlareEvent = {}
+    try:
+        stream = StreamReader()
+        stream.feed_data(data)
+        stream.feed_eof()
+        async for part in MultipartReader(headers, stream):
+            if part.name is not None:
+                if part.name == "flare_file":
+                    tracer_flare[part.name] = base64.b64encode(await part.read()).decode("ascii")
+                else:
+                    tracer_flare[part.name] = await part.text()
+    except Exception as err:
+        tracer_flare["error"] = str(err)
+    return tracer_flare
