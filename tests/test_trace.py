b8fb6999b7935e2febcc8fd8054c22d6ec3b3ec4
Andrew Glaude <andrew.glaude@datadoghq.com>
2025-08-20 16:10:21 -0400

    Remove unused import

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -4,7 +4,6 @@ import msgpack
 import pytest
 
 from ddapm_test_agent.trace import bfs_order
-from ddapm_test_agent.trace import decode_v1_span
 from ddapm_test_agent.trace import decode_v04
 from ddapm_test_agent.trace import dfs_order
 from ddapm_test_agent.trace import root_span
