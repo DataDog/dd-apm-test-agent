644f5bd1fef8f2ae8168a1a8f6f32b6fbb19f38f
vianney <vianney.ruhlmann@datadoghq.com>
2025-09-17 11:12:54 +0200

    Remove Datadog-Agent-State header from /info endpoint response (#248)
    * remove agent state header to match agent
    
    * Add release note

--- ddapm_test_agent/agent.py
+++ ddapm_test_agent/agent.py
@@ -831,7 +831,6 @@ class Agent:
                 "peer_tags": ["db.name", "mongodb.db", "messaging.system"],
                 "span_events": True,  # Advertise support for the top-level Span field for Span Events
             },
-            headers={"Datadog-Agent-State": "03e868b3ecdd62a91423cc4c3917d0d151fb9fa486736911ab7f5a0750c63824"},
         )
 
     async def _handle_traces(self, request: Request, version: Literal["v0.4", "v0.5", "v0.7", "v1"]) -> web.Response:
