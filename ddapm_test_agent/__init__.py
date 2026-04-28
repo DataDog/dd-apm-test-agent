8b7915d2714820bc5625a1fb9be3da75ae014c9e
Sam Brenner <sam.brenner@datadoghq.com>
2026-04-28 13:00:21 -0400

    modernize _get_version

--- ddapm_test_agent/__init__.py
+++ ddapm_test_agent/__init__.py
@@ -1,4 +1,10 @@
-def _get_version():
-    import pkg_resources
+def _get_version() -> str:
+    from importlib.metadata import PackageNotFoundError
+    from importlib.metadata import version
 
-    return pkg_resources.get_distribution(__name__).version
+    try:
+        return version(__name__)
+    except PackageNotFoundError:
+        import pkg_resources
+
+        return pkg_resources.get_distribution(__name__).version
