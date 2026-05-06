6a6c4b08cceb8618e8a276eb22382fef9a4b8196
Sam Brenner <sam.brenner@datadoghq.com>
2026-05-06 11:10:21 -0400

    fix logging from bootstrap

--- lapdog/bootstrap/sitecustomize.py
+++ lapdog/bootstrap/sitecustomize.py
@@ -17,21 +17,19 @@ except ImportError:
         except ImportError:
             _lapdog_ver = os.environ.get("_LAPDOG_PYTHON_VERSION", "unknown")
             _target_ver = f"{sys.version_info.major}.{sys.version_info.minor}"
-            print(
+            os.write(2, (
                 f"[lapdog] Python version mismatch: lapdog uses Python {_lapdog_ver} "
                 f"but this process is Python {_target_ver}.\n"
                 "[lapdog] Install ddtrace in your app's Python environment:\n"
-                "[lapdog]   pip install ddtrace",
-                file=sys.stderr,
-            )
+                "[lapdog]   pip install ddtrace\n"
+            ).encode())
             os._exit(1)
     else:
-        print(
+        os.write(2, (
             "[lapdog] ddtrace is not installed.\n"
             "[lapdog] Install it in your app's Python environment:\n"
-            "[lapdog]   pip install ddtrace",
-            file=sys.stderr,
-        )
+            "[lapdog]   pip install ddtrace\n"
+        ).encode())
         os._exit(1)
 
 import ddtrace.auto  # noqa: F401
